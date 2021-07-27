1fee11abfcc1c2534db0cf1605f3d40a881d8655
Erick Borel <erickborel@hotmail.com>
2021-07-27 20:15:57 -0300

    melhora do repositório

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 function converterDolar() {
+        
     var valorDolar = parseFloat(document.getElementById("valor-dolar").value)
-    if (valorDolar > 0) {
-        var valorReal = valorDolar * 5.09
-        document.getElementById("resultado-real").innerHTML = valorReal.toFixed(2)
-    } else {
-        document.getElementById("resultado-real").innerHTML = "Adicione um valor!"
-    }   
-}
+        if (valorDolar > 0) {
+            var valorReal = valorDolar * 5.09
+            document.getElementById("resultado-real").innerHTML = valorReal.toFixed(2)
+        } else {
+            document.getElementById("resultado-real").innerHTML = "Adicione um valor!"
+        }   
+    }
